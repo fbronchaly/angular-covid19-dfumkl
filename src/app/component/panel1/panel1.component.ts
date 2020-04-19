a529c9fb478c899764e797c71bb9cfc45e292359
fbronchaly <fbronchaly@gmail.com>
2020-04-19 09:30:43 +0200

    2 files modified

--- src/app/component/panel1/panel1.component.ts
+++ src/app/component/panel1/panel1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 
@@ -10,11 +11,16 @@ import { Component, OnInit } from '@angular/core';
 export class Panel1Component implements OnInit {
 
 
-  constructor() { }
+  constructor(private router: Router) {
+   }
 
   ngOnInit() {
-  }
+    }
 
- 
+  
+    salto(){
+
+    this.router.navigate(['/profile-nefro']);
+}
 
 }
